6890665546845d83a244dcb49f50a1d80c0d2126
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-11-08 16:52:37 +0100

    added some content, made button, adjusted header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-const url = "https://www.aservify.no/wp-json/wp/v2/posts";
-
-
 async function getPosts() {
+  const url = "https://www.aservify.no/wp-json/wp/v2/posts";
+  
   const response = await fetch(url);
   const posts = await response.json();
 
@@ -10,7 +9,7 @@ async function getPosts() {
     let formattedDate = new Date(Date.parse(posts[i].date))
     formattedDate = formattedDate.toLocaleDateString()
 
-    postCard.innerHTML += `<img src="${posts[i].jetpack_featured_media_url}" class="wp-image-86">
+    postCard.innerHTML += `<img src="${posts[i].jetpack_featured_media_url}" alt="#">
                             <div>
                             <p>${formattedDate}</p>
                             <h2>${posts[i].title.rendered}</h2>
@@ -25,3 +24,38 @@ async function getPosts() {
 
 getPosts();
 
+
+
+
+function showNavMenu() {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+
+  hamburgerIcon.addEventListener("click", event => {
+
+    const navMenuContainer = document.createElement("div");
+    document.body.appendChild(navMenuContainer)
+
+    const navMenu1 = document.createElement("p");
+    navMenu1.innerText = "HOME";
+    navMenuContainer.appendChild(navMenu1)
+
+    const navMenu2 = document.createElement("p");
+    navMenu2.innerText = "BLOG";
+    navMenuContainer.appendChild(navMenu2)
+
+    const navMenu3 = document.createElement("p");
+    navMenu3.innerText = "</>CODE";
+    navMenuContainer.appendChild(navMenu3)
+
+    const navMenu4 = document.createElement("p");
+    navMenu4.innerText = "CONTACT";
+    navMenuContainer.appendChild(navMenu4)
+
+    const navMenu5 = document.createElement("p");
+    navMenu5.innerText = "ABOUT";
+    navMenuContainer.appendChild(navMenu5)
+    
+  })
+}
+
+showNavMenu();
