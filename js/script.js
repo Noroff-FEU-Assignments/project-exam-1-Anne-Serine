59d86b4a9873f37d6806bdca794af03e6bb5180c
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-11-10 14:21:23 +0100

    adjusted header, made it responsive

--- js/script.js
+++ js/script.js
@@ -29,33 +29,38 @@ getPosts();
 
 function showNavMenu() {
   const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const headerNav = document.querySelector("#headerNav");
 
   hamburgerIcon.addEventListener("click", event => {
-
-    const navMenuContainer = document.createElement("div");
-    document.body.appendChild(navMenuContainer)
-
-    const navMenu1 = document.createElement("p");
-    navMenu1.innerText = "HOME";
-    navMenuContainer.appendChild(navMenu1)
-
-    const navMenu2 = document.createElement("p");
-    navMenu2.innerText = "BLOG";
-    navMenuContainer.appendChild(navMenu2)
-
-    const navMenu3 = document.createElement("p");
-    navMenu3.innerText = "</>CODE";
-    navMenuContainer.appendChild(navMenu3)
-
-    const navMenu4 = document.createElement("p");
-    navMenu4.innerText = "CONTACT";
-    navMenuContainer.appendChild(navMenu4)
-
-    const navMenu5 = document.createElement("p");
-    navMenu5.innerText = "ABOUT";
-    navMenuContainer.appendChild(navMenu5)
-    
+    headerNav.classList.toggle("active");
+   
   })
 }
 
 showNavMenu();
+
+
+
+
+// const navMenuContainer = document.createElement("div");
+// document.body.appendChild(navMenuContainer)
+
+// const navMenu1 = document.createElement("p");
+// navMenu1.innerText = "HOME";
+// navMenuContainer.appendChild(navMenu1)
+
+// const navMenu2 = document.createElement("p");
+// navMenu2.innerText = "BLOG";
+// navMenuContainer.appendChild(navMenu2)
+
+// const navMenu3 = document.createElement("p");
+// navMenu3.innerText = "</>CODE";
+// navMenuContainer.appendChild(navMenu3)
+
+// const navMenu4 = document.createElement("p");
+// navMenu4.innerText = "CONTACT";
+// navMenuContainer.appendChild(navMenu4)
+
+// const navMenu5 = document.createElement("p");
+// navMenu5.innerText = "ABOUT";
+// navMenuContainer.appendChild(navMenu5)
