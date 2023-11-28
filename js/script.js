622fb6109bf10dc1c0bcf91348cf4a858eec5d35
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-11-29 00:04:09 +0100

    adjust css

--- js/script.js
+++ js/script.js
@@ -511,13 +511,13 @@ if(aboutMeContainer) {
 
 
 let coffeeFacts = [
-  "Approximately 400 billion cups of coffee are consumed every year",
-  "It takes 100 beans to make 1 cup of coffee",
-  "It takes 5 years for a coffee tree to mature enough to produce its first crop",
+  "Approximately 400 billion cups of coffee are consumed every year.",
+  "It takes 100 beans to make 1 cup of coffee.",
+  "It takes 5 years for a coffee tree to mature enough to produce its first crop.",
   "Once roasted a coffee bean increases in volume but decreases in weight (Like popcorn!)",
-  "Coffee consists of 98.5% water",
-  "Light-roasted coffee contains more caffeine than dark-roasted coffee",
-  "Coffee is a fruit. ",
+  "Coffee consists of 98.5% water.",
+  "Light-roasted coffee contains more caffeine than dark-roasted coffee.",
+  "Coffee beans: While they do look a lot like beans, coffee “beans” are actually the seed, or pit, of the fruit that grows on coffee trees. Coffee trees grow small, bright red fruit called coffee cherries.",
 ]
 
 const coffeeFactContainer = document.querySelector(".coffee-fact-container");
